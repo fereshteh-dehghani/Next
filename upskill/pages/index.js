850c85fefdd30231015ef19c8549379ec01a48c3
fereshteh_dehghani <fdehghani666@gmail.com>
2022-11-07 18:49:44 +0330

    routing

--- upskill/pages/index.js
+++ upskill/pages/index.js
@@ -4,27 +4,85 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
-export default function Home() {
-const [todos,setTodos] = useState([]);
-useEffect(()=>{
-  const fetchTodos = async () =>{
-   setTimeout(async () =>{
-    const res= await fetch('https://jsonplaceholder.typicode.com/todos');
-    const data = await res.json();
-    setTodos(data);
-   },3000)
+// export async function getServerSideProps (){
+//   const res = await fetch('https://jsonplaceholder.typicode.com/todos');
+//   const data = await res.json();
+
+//   return{
+//     props:{
+//       todos:data
+//     }
+//   }
+// }
+
+
+export async function  getServerSideProps () {
+  const res = await  fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json');
+  // const data = res.json();
+
+  return{
+    props:{
+      pokemon:await res.json(),
+    },
   }
-  fetchTodos();
-},[])
+}
+
+export default function Home({pokemon}) {
+// const [pokemon,setpokemon] = useState([]);
+
+// useEffect(()=>{
+//   const fetchPokemon = async () =>{
+//     const res= await fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json');
+//     const data = await  res.json();
+//     setpokemon(data);
+//   }
+
+
+//   fetchPokemon();
+// },[])
+
+// const [todos,setTodos] = useState([]);
+// useEffect(()=>{
+//   const fetchTodos = async () =>{
+//    setTimeout(async () =>{
+//     const res= await fetch('https://jsonplaceholder.typicode.com/todos');
+//     const data = await res.json();
+//     setTodos(data);
+//    },3000)
+//   }
+//   fetchTodos();
+// },[])
+
 
   return (
     <div className={styles.container}>
      <Navbar/>
    <div>
    <h1>Hackaton aps</h1>
-   {todos.length===0 ? (
+<div className={styles.grid}>
+{pokemon.length===0 ? (
+  <div>
+    <p>Loading...</p>
+  </div>
+):(
+  pokemon.map(poke => (
+    <div className={styles.card} key={poke.id}>
+    <Link href={`pokemon/${poke.id}`}>
+      <img
+      src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${poke.image}`}
+      />
+      <h3>{poke.name}</h3>
+
+    </Link>
+  </div>
+  ))
+)}
+</div>
+
+   {/* {todos.length===0 ? (
     <div>Loading...</div>
    ):(
     todos.map(todo =>(
@@ -35,7 +93,7 @@ useEffect(()=>{
       </div>
     )
     )
-   )}
+   )} */}
     <Link href="/hackaton/">
    Hackaton Tasks
    </Link>
---
+++ upskill/pages/products/[ProductId].js
@@ -0,0 +1,9 @@
+const ProductDetails = () => {
+    return ( 
+        <>
+        <h2>This is about ProductDetails</h2>
+        </>
+     );
+}
+ 
+export default ProductDetails;
